35371c005044fcd2e430875850ff4994a625b27c
slowww <kenotic87@gmail.com>
2019-04-04 18:53:55 +0200

    aggiunto html/ajax per accesso api

--- src/Servlet.java
+++ src/Servlet.java
@@ -6,8 +6,10 @@ import org.json.JSONObject;
 import org.mariadb.jdbc.MariaDbDataSource;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.WebEndpoint;
 import java.io.IOException;
 import java.sql.*;
 import java.util.Enumeration;
@@ -345,6 +347,7 @@ public class Servlet extends javax.servlet.http.HttpServlet {
 
 
     @SuppressWarnings("Duplicates")
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         int count;
